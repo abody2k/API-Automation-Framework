fed61c2600b06fdf9c17c547d79dccca6d5829a9
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-12 18:35:59 +0300

    added login data samples, and tested all possible login cases

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { loginSchema } from '../schemas/auth.schema';
 import { login } from '../api/auth.api';
+import { login_data } from '../data/auth_sample.data';
 
 
 test.describe("Auth tests", () => {
@@ -11,8 +12,8 @@ test.describe("Auth tests", () => {
 
   test("Login with valid username and password", async ({ }) => {
 
-    let data = await login({ username: process.env.LOGIN_USERNAME as string, password: process.env.PASSWORD as string})
-    
+    let data = await login({ username: process.env.LOGIN_USERNAME as string, password: process.env.PASSWORD as string })
+
     let schemaResult = loginSchema.safeParse(data)
 
     if (!schemaResult.success) {
@@ -22,6 +23,26 @@ test.describe("Auth tests", () => {
     expect(schemaResult.success).toBeTruthy();
 
   })
+
+
+
+
+  login_data.forEach((dataItem) => {
+
+    test(`Login with ${dataItem.msg}`, async ({ }) => {
+
+      let data = await login({ username: dataItem.username, password: dataItem.password })
+
+      let schemaResult = loginSchema.safeParse(data)
+
+
+      expect(schemaResult.success).toBeFalsy();
+    })
+  })
+
+
+
+
 
 
 
