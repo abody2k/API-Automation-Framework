be7eb95900f1ff9214d1e2a9d5aa5f61ca2262d2
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-12 17:52:30 +0300

    added schemas directory, and implemented login schema

---
+++ api/auth.api.ts
@@ -0,0 +1,33 @@
+import { request } from "playwright";
+import * as zod from 'zod'
+import { loginSchema } from "../schemas/auth.schema";
+/**
+ * login function that takes username and password and returns a token
+ * @param username 
+ * @param password 
+ */
+export async function login(username: string, password: string) {
+
+    
+
+
+    let context = await request.newContext({
+        baseURL: process.env.BASE_URL,
+        extraHTTPHeaders: {
+            "Content-Type": "application/json"
+        },
+    });
+
+    let data = await (await context.post("/auth/login", {
+
+        data: {
+            username: username,
+            password: password
+        }
+    })).json();
+
+    console.log(loginSchema.safeParse(data).success);
+
+
+
+}
--- tests/init.ts
+++ tests/init.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import path from 'node:path';
 import { getAllUsers } from '../utils/users.util';
+import { login } from '../api/auth.api';
 
 dotenv.config({path: path.resolve(__dirname,"../.env")})
 
 
-getAllUsers()
+login("emilys","emilyspass")
