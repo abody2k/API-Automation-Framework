f3ec91ae3180658355589b2ad8317f9c4cf485c9
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-04-14 10:39:29 +0300

    Added deleting user edge cases like not sending ID or sending a faulty ID

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -21,21 +21,35 @@ test.describe("Users related tests", () => {
 
 
 
+    test("Deleting a user using an invalid ID", { annotation: { type: "edge", description: "The response should be not found" } }, async () => {
 
+        let req = await deleteUser("5676567")
+        await checkResponse({ response: req, statusCode: 404, statusText: RESPONSE_STATUS.NOT_FOUND })
+
+
+    })
+
+    test("Deleting a user without sending an ID", { annotation: { type: "edge", description: "The response should be not found" } }, async () => {
+
+        let req = await deleteUser()
+        await checkResponse({ response: req, statusCode: 400, statusText: RESPONSE_STATUS.BAD_REQUEST })
+
+
+    })
 
     test("Updating a user's info using their ID", async () => {
 
 
 
 
-        let res = await updateUser(1,{
+        let res = await updateUser(1, {
 
 
-            firstName:"Anderson"
+            firstName: "Anderson"
         })
 
 
-        await checkResponse({response:res,statusCode:200,statusText:RESPONSE_STATUS.OK})
-        
+        await checkResponse({ response: res, statusCode: 200, statusText: RESPONSE_STATUS.OK })
+
     })
 })
